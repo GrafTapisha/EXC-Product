5703fea9d6ec53887fe0dfd570e71114d478ca39
Aleksandr <graftapisha@gmail.com>
2023-02-18 15:36:59 +0500

    initol

---
+++ src/main/java/ProductRepository.java
@@ -0,0 +1,42 @@
+public class ProductRepository {
+    Product[] products = new Product[0];
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public void removeById(int reId) {
+        if (findById(reId) == null) {
+            throw new NotFoundException(reId);
+        }
+
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != reId) {
+                tmp[copyToIndex] = product;
+            }
+            copyToIndex++;
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+}
